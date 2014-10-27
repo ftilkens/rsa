6cb7c53a7fb0d31aa667f0f33670ed6eed4ae891
ftilkens <filip.tilkens@telenet.be>
2014-10-27 16:33:02 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rsa::Application.config.secret_key_base = 'b6d4e0f2800d1125b20f4e13635912ba5ea7e8e920c603112796d0d4f5f635f7a1a603786ced02de4d8dc9f3dde384ef1780a4d06ae8997d57fb76593900623e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rsa::Application.config.session_store :cookie_store, key: '_rsa_session'
